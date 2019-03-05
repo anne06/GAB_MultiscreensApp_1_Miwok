7a06f73779ed653a3cfce09f42f262e6c817b0eb
anne6 <anne.chatin@gmail.com>
2019-03-05 15:23:23 -0800

    Display list with a LINEAR LAYOUT: NO optimisation

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -1,13 +1,42 @@
 package com.example.android.miwok;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class NumbersActivity extends AppCompatActivity {
+    private final static String LOG_TAG = NumbersActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        // Creation of the worlds list
+        List<String> words = new ArrayList<>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+        // Display all those words on the screen
+        LinearLayout rootLL = (LinearLayout) findViewById(R.id.rootView);
+        TextView wordView;
+        for (int index=0; index < words.size(); index++){
+            wordView = new TextView(this);
+            wordView.setText(words.get(index));
+            rootLL.addView(wordView);
+        }
+
     }
 }
