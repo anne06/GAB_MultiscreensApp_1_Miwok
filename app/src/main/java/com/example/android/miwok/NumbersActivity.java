58b40cc53009e85e51725065ebfe9d01377952c1
anne6 <anne.chatin@gmail.com>
2019-03-12 10:12:27 -0700

    End sound part with arrow play

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -68,7 +68,7 @@ public class NumbersActivity extends AppCompatActivity {
         final List<Word> words = new ArrayList<>();
         words.add(new Word("one", "lutti", R.drawable.number_one, R.raw.number_one));
         words.add(new Word("two", "otiiko", R.drawable.number_two, R.raw.number_two));
-      /*  words.add(new Word("three", "tolookosu", R.drawable.number_three, R.raw.number_three));
+        words.add(new Word("three", "tolookosu", R.drawable.number_three, R.raw.number_three));
         words.add(new Word("four", "oyyisa", R.drawable.number_four, R.raw.number_four));
         words.add(new Word("five", "massokka", R.drawable.number_five, R.raw.number_five));
         words.add(new Word("six", "temmokka", R.drawable.number_six, R.raw.number_six));
@@ -76,7 +76,7 @@ public class NumbersActivity extends AppCompatActivity {
         words.add(new Word("eight", "kawinta", R.drawable.number_eight, R.raw.number_eight));
         words.add(new Word("nine", "wo'e", R.drawable.number_nine, R.raw.number_nine));
         words.add(new Word("ten", "na'aacha", R.drawable.number_ten, R.raw.number_ten));
-*/
+
 
         // Create an {@link ArrayAdapter}, whose data source is a list of Strings. The
         // adapter knows how to create layouts for each item in the list, using the
