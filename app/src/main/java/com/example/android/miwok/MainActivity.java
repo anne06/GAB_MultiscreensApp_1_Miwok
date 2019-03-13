8135a2895a165201f8f971077c3c79d4d1d4d288
anne6 <anne.chatin@gmail.com>
2019-03-13 16:36:18 -0700

    Added Viewpager and FragmentPagerAdapter - All aprevious activities are removed

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -1,11 +1,8 @@
 package com.example.android.miwok;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,54 +11,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Start NUMBERS activity
-        TextView numbers = findViewById(R.id.numbers);
-        numbers.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(v.getContext(), "Numbers", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(MainActivity.this, NumbersActivity.class));
-            }
-        });
-
-        // Start FAMILY MEMBERS activity
-        TextView family = findViewById(R.id.family);
-        family.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(v.getContext(), "Family", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(MainActivity.this, FamilyActivity.class));
-            }
-        });
-
-        // start COLORS activity
-        TextView colors = findViewById(R.id.colors);
-        colors.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(v.getContext(), "Colors", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(MainActivity.this, ColorsActivity.class));
-            }
-        });
-
-        // start PHRASES activity
-        TextView phrases = findViewById(R.id.phrases);
-        phrases.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(v.getContext(), "Phrases", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(MainActivity.this, PhrasesActivity.class));
-            }
-        });
-
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        FixedTabsPagerAdapter pagerAdapter = new FixedTabsPagerAdapter(this, getSupportFragmentManager());
+        viewPager.setAdapter(pagerAdapter);
     }
-
-
-    /*
-    public void openNumbersList(View view){
-        // EXPLICIT intent
-        Intent intent = new Intent(this, NumbersActivity.class);
-        startActivity(intent);
-    }
-    */
 }
